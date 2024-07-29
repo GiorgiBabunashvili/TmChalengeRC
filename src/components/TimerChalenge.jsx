cdb36decb80fd26d50b723d9a023061f8490f754
shanME <giorgie103188@gmail.com>
2024-07-29 15:53:51 +0400

    cleartimeout

--- src/components/TimerChalenge.jsx
+++ src/components/TimerChalenge.jsx
@@ -1,14 +1,36 @@
+import { useRef, useState } from "react";
+
 const TimerChalenge = ({ title, targetTime }) => {
+  const timer = useRef();
+  const [timerExpired, setTimerExpired] = useState(false);
+  const [timerStarted, setTimerStarted] = useState(false);
+
+  function hanldeStart() {
+    setTimerStarted(true);
+    timer.current = setTimeout(() => {
+      setTimerExpired(true);
+    }, targetTime * 1000);
+  }
+
+  function hanldeStop() {
+    clearTimeout(timer.current);
+  }
+
   return (
     <section className="challenge">
       <h2>{title}</h2>
+      {timerExpired && <p>You lost!</p>}
       <p className="challenge-time">
         {targetTime} second{targetTime > 1 ? "s" : ""}
       </p>
       <p>
-        <button>Start Challenge</button>
+        <button onClick={timerStarted ? hanldeStop : hanldeStart}>
+          {timerStarted ? "Stop" : "Start"} Challenge
+        </button>
       </p>
-      <p className="">Time is running / Timer inactive</p>
+      <p className={timerStarted ? "active" : undefined}>
+        {timerStarted ? "Time is running..." : "Timer inactive"}
+      </p>
     </section>
   );
 };
