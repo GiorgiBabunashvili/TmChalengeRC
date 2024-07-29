11b6c6c0cf9fc052f1ed2709f78471bf07288042
shanME <giorgie103188@gmail.com>
2024-07-29 15:43:43 +0400

    start chellenge function

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import Player from "./components/Player.jsx";
+import TimerChalenge from "./components/TimerChalenge.jsx";
 
 function App() {
   return (
     <>
       <Player />
-      <div id="challenges"></div>
+      <div id="challenges">
+        <TimerChalenge title="Easy" targetTime={1} />
+        <TimerChalenge title="Not easy" targetTime={5} />
+        <TimerChalenge title="getting tough" targetTime={10} />
+        <TimerChalenge title="Pros only" targetTime={15} />
+      </div>
     </>
   );
 }
