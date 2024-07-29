83dd1e09883317306122b125f6c4e27fd6fda4a2
shanME <giorgie103188@gmail.com>
2024-07-29 13:43:35 +0400

    i react

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Player from "./components/Player.jsx";
+
+function App() {
+  return (
+    <>
+      <Player />
+      <div id="challenges">hello world</div>
+    </>
+  );
+}
+
+export default App;
